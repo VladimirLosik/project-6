c7b30582ecc6e283136c6642803abb3dbd9abf6a
VladimirLosik <jazz-vovanchik@yandex.ru>
2022-03-29 15:47:08 +0300

    2.1

---
+++ js.js
@@ -0,0 +1,13 @@
+function PopUpShow(){
+    $("#popup1").show();
+}
+function PopUpHide(){
+    $("#popup1").hide();
+}
+
+function PopUpShow(){
+    $("#popup2").show();
+}
+function PopUpHide(){
+    $("#popup2").hide();
+}
